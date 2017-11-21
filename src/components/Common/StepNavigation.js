981cf99e8b5e195e0c578326a283340343ba774c
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2017-11-21 15:45:29 +0300

    Update StepNavigation.js
    Translated

--- src/components/Common/StepNavigation.js
+++ src/components/Common/StepNavigation.js
@@ -7,11 +7,11 @@ const { CROWDSALE_CONTRACT, TOKEN_SETUP, CROWDSALE_SETUP, PUBLISH, CROWDSALE_PAG
 export const StepNavigation = ({activeStep}) => (
 	<div className="steps-navigation">
 		<div className="container">
-			<div className={getStepClass(CROWDSALE_CONTRACT, activeStep)}>Crowdsale Contract</div>
-			<div className={getStepClass(TOKEN_SETUP, activeStep)}>Token Setup</div>
-			<div className={getStepClass(CROWDSALE_SETUP, activeStep)}>Crowdsale Setup</div>
-			<div className={getStepClass(PUBLISH, activeStep)}>Publish</div>
-			<div className={getStepClass(CROWDSALE_PAGE, activeStep)}>Crowdsale Page</div>
+			<div className={getStepClass(CROWDSALE_CONTRACT, activeStep)}>Контракт кампании(ICO)</div>
+			<div className={getStepClass(TOKEN_SETUP, activeStep)}>Параметры токена</div>
+			<div className={getStepClass(CROWDSALE_SETUP, activeStep)}>Параметры кампании</div>
+			<div className={getStepClass(PUBLISH, activeStep)}>Публикация</div>
+			<div className={getStepClass(CROWDSALE_PAGE, activeStep)}>Страница кампании</div>
 		</div>
 	</div>
 )
