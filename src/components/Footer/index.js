7dc66424b4af5ef1f6fe9fc1845527b37d643ccb
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2017-11-24 14:51:40 +0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 export const Footer = () => (
 	<footer className="footer">
 		<div className="container">
-			<p className="rights">2017 Oracles Network. All rights reserved.</p>
-      <Link className="logo" to='/'></Link>
+			<p className="rights">2017 Oracles Network. Russian by Allsmartcontracts.ru All rights reserved.</p>
+     
 			<div className="socials">
-			  <a href="https://twitter.com/oraclesorg" className="social social_twitter"></a>
-        <a href="https://www.oracles.org" className="social social_oracles"></a>
-        <a href="https://t.me/oraclesnetwork" className="social social_telegram"></a>
-        <a href="https://github.com/oraclesorg/" className="social social_github"></a>
+			  <a href="https://twitter.com/" className="social social_twitter"></a>
+        <a href="http://www.allsmartcontracts.ru" className="social social_oracles"></a>
+        <a href="https://t.me" className="social social_telegram"></a>
+        <a href="https://github.com/" className="social social_github"></a>
 			</div>
 		</div>
 	</footer>
