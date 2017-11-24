992a86c1e063bffea00ade83e06a1235b0f18a9f
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-11-24 15:07:15 +0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const Footer = () => (
 	<footer className="footer">
 		<div className="container">
-			<p className="rights">2017 Oracles Network. Russian by Allsmartcontracts.ru All rights reserved.</p>
+			<p className="rights">2017 Oracles Network. Russian by Allsmartcontracts.ru</p>
      
 			<div className="socials">
 			  <a href="https://twitter.com/" className="social social_twitter"></a>
