2cc2b2dd547e9fe26341d4a13854c6e449255a94
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2017-11-28 22:00:45 +0300

    Update config.js

--- src/components/Common/config.js
+++ src/components/Common/config.js
@@ -6,9 +6,6 @@ const networks = {
 	kovan: 42,
 	oraclesTest: 12648430
 }
-export const ICOConfig = { 
-	crowdsaleContractURL: '0x73e48E5479AE0450418777CeaaebA02F003ab14',
-	networkID: networks.kovan
-};
+export const ICOConfig = {};
 
 
