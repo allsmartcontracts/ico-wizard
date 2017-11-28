84cf297b096f096c97ac44c6c5b4151e36aae6e3
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2017-11-29 02:25:17 +0300

    Update QRPaymentProcess.js

--- src/components/invest/QRPaymentProcess.js
+++ src/components/invest/QRPaymentProcess.js
@@ -14,14 +14,14 @@ const QRPaymentProcess = ({ crowdsaleAddress }) => {
         </p>
 
         <CopyToClipboard text={crowdsaleAddress}>
-          <a href="" onClick={e => e.preventDefault()} className="payment-process-copy">Copy Address</a>
+          <a href="" onClick={e => e.preventDefault()} className="payment-process-copy">Копировать адрес</a>
         </CopyToClipboard>
 
         {/* <div className="payment-process-loader">Waiting for payment</div> */}
         <div className="payment-process-notation">
-          <p className="payment-process-notation-title">Important</p>
+          <p className="payment-process-notation-title">ВНИМАНИЕ!</p>
           <p className="payment-process-notation-description">
-            Send ethers to the crowdsale address with a MethodID: 0xa6f2ae3a
+            Отправляйте ETH только с указанием MethodID: 0xa6f2ae3a
           </p>
         </div>
       </div>
