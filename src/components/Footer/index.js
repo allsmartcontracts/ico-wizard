f92c3f00091c8a07d52e18308266f2f995ccb9e2
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2017-12-06 00:09:52 +0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,12 @@ import { Link } from 'react-router-dom'
 export const Footer = () => (
 	<footer className="footer">
 		<div className="container">
-			<p className="rights">2017 Oracles Network. Russian by Allsmartcontracts.ru All rights reserved.</p>
+			<p className="rights">2017 Oracles Network.
+	                <br/>Russian by YouTweak.It All rights reserved.
+	                </p>
      
 			<div className="socials">
-        <a href="https://github.com/PowerMiningSystem" className="social social_github"></a>
+        <a href="https://github.com/youtweakit" className="social social_github"></a>
 			</div>
 		</div>
 	</footer>
