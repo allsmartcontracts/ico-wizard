72edc74f0c54455432774b2338bdc3bdc34931c9
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-12-05 23:37:45 +0300

    Update blockchainHelpers.js

--- src/utils/blockchainHelpers.js
+++ src/utils/blockchainHelpers.js
@@ -2,7 +2,9 @@ import Web3 from 'web3';
 import { incorrectNetworkAlert, noMetaMaskAlert, invalidNetworkIDAlert } from './alerts'
 import { getEncodedABIClientSide } from './microservices'
 import { GAS_PRICE, CHAINS } from './constants'
-import {networkID} from 'ICOConfig'
+import { ICOConfig } from '../../src/components/Common/config'
+import { getQueryVariable} from './utils'
+
 
 // instantiate new web3 instance
 const web3 = new Web3();
@@ -47,17 +49,22 @@ export function getWeb3(cb) {
     cb(web3, false);
   } else {
 	  //get current network
-	  if (web3.currentProvider!=ICOconfig.NetworkID){
-		  const infuraID="Https://{ICODconfig.NetworkID}.infura.io/DI0Ayd5pD4v0mFuuR3Zn";
-		  }
+	  const networkID = ICOConfig.networkID?ICOConfig.networkID:getQueryVariable("networkID");
+	  if (networkName!='ORACLES'){
+	  var networkName = getNetWorkNameById(networkID);
+          let infuraID='https://${networkName}.infura.io/DI0Ayd5pD4v0mFuuR3Zn';
+		  
 	  var cnetwork
     // window.web3 == web3 most of the time. Don't override the provided,
     // web3, just wrap it in your Web3. adding INFURA for NoMetamsk
-    var InfuraConnect = new Web3(web3.provider.HttpProvider({infuraID}));
+    var web3 = new Web3(new Web3.providers.HttpProvider('{infuraID}'));
 
-    cb(InfuraConnect, false);
+    cb(web3, false);
   }
-  return InfuraConnect;
+  return web3;
+  } else {
+	  console.error("Please use a web3 browser");}
+	  
 }
 
 export function checkNetWorkByID(web3, _networkIdFromGET) {
