d066bf3e50c82a60520168efd7dadeafe013d6c9
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-12-05 20:46:18 +0300

    Update blockchainHelpers.js

--- src/utils/blockchainHelpers.js
+++ src/utils/blockchainHelpers.js
@@ -48,7 +48,7 @@ export function getWeb3(cb) {
     cb(web3, false);
   } else {
 	  //get current network
-	  const networkName = getNetWorkNameById(ICOConfig.networkID);
+	  var networkName = getNetWorkNameById(ICOConfig.networkID);
           const infuraID="https://"+{networkName}+".infura.io/DI0Ayd5pD4v0mFuuR3Zn";
 		  
 	  var cnetwork
