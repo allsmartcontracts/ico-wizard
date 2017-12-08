8701e15401b9124a516aee583379062f4b28cbe3
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-12-08 19:35:10 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,14 @@ import { Link } from 'react-router-dom'
 export const Header = () => (
 	<header className="header">
     <div className="container">
-      <Link className="logo" to='/'></Link>
+     const Toolbar = ({locale}) => <div>
+    <select value={locale.value} onChange={(event) => locale.value = event.target.value}>
+        <option value="de">Russian</option>
+        <option value="en">English</option>
+    </select>
+</div>
+
+export default inject("locale")(observer(Toolbar));
     </div>
   </header>
 )
