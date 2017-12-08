ccf3c3bfad9ef8df05b7051910cfce8decb133e5
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-12-08 21:56:03 +0300

    Update en.js

--- src/translations/en.js
+++ src/translations/en.js
@@ -1,7 +1,7 @@
 export default {
                       "Home_title1": "Ico Wizard",
                       "Home_desc1": "ICO Wizard is a client side tool to create token and crowdsale contracts in five steps. It helps you to publish contracts on the Ethereum network, verify them in Etherscan, create a crowdsale page with stats. For participants, the wizard creates a page to invest into the campaign.",
-                      "Home_desc11": "Smart contracts based on <a href="https://github.com/TokenMarketNet/ico">TokenMarket</a> contracts.",
+                      "Home_desc11": "Smart contracts based on TokenMarket contracts.",
                       "Home_butt1": "New crowdsale",
                       "Home_title_contract": "Crowdsale Contract",
                       "Home_desc_contract": "Select a strategy for crowdsale contract",
