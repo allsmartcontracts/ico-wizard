0ccd38924174197dfe6d571441f5887516a79271
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-12-08 19:55:14 +0300

    Update index.js

--- src/components/crowdsale/index.js
+++ src/components/crowdsale/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../assets/stylesheets/application.css'
+import {Toolbar} from '../Header/index.js';
 import { checkNetWorkByID, checkWeb3 } from '../../utils/blockchainHelpers'
 import {
   findCurrentContractRecursively,
