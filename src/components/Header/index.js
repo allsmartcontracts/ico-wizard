9b23fd9fa4c451ba02d03b6a51c2b2b6bfd66fa7
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-12-09 22:37:15 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import '../../assets/stylesheets/application.css';
 import { Link } from 'react-router-dom'
-import {inject, observer} from "mobx-react" 
-@inject('locale') @observer
 
-export class Header = () => (
-	<header className="socials">
+export const Header = () => (
+	<header className="header">
     <div className="container">
-     const Toolbar = ({locale}) => <div>
-    <select value={locale.value} onChange={(event) => locale.value = event.target.value}>
-        <option value="de">Russian</option>
-        <option value="en">English</option>
-    </select>
-</div>
-
+      <Link className="logo" to='/'></Link>
     </div>
   </header>
 )
-
