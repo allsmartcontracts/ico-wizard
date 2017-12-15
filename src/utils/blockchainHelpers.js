b57f2759e41e323b22dbd6d75a94541b33fe44b1
allsmartcontracts <33957163+allsmartcontracts@users.noreply.github.com>
2017-12-15 18:26:47 +0300

    Update blockchainHelpers.js

--- src/utils/blockchainHelpers.js
+++ src/utils/blockchainHelpers.js
@@ -50,14 +50,15 @@ export function getWeb3(cb) {
   } else {
 	  //get current network
 	  const networkID = ICOConfig.networkID?ICOConfig.networkID:getQueryVariable("networkID");
+	  const InfToken=ICOConfig.InfuraToken;
 	  if (networkName!='ORACLES'){
 	  var networkName = getNetWorkNameById("networkID");
-          let infuraID="https://${networkName}.infura.io/DI0Ayd5pD4v0mFuuR3Zn";
+          let infuraID="https://" + networkName + ".infura.io/" + InfToken;
 		  
 	  var cnetwork
     // window.web3 == web3 most of the time. Don't override the provided,
     // web3, just wrap it in your Web3. adding INFURA for NoMetamsk
-    var infweb3 = new Web3(new Web3.providers.HttpProvider('${infuraID}'));
+    var infweb3 = new Web3(new Web3.providers.HttpProvider('infuraID'));
     var web3=infweb3;
     cb(web3, false);
   }
